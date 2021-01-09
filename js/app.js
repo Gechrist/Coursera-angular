a2a08330bbc31d10f74826524f03c195d2b40739
stef-christ <stefanosc@yahoo.com>
2021-01-09 11:41:41 +0200

    first module solution

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+  $scope.text = "";
+  $scope.message= "";
+  $scope.check = function () {
+    if ($scope.text.length == 0) {
+        $scope.message = "Please enter data first";
+        }
+    else {
+      var  list = $scope.text.split(",")
+      if (list.length <= 3) {
+        $scope.message = "Enjoy!";
+      }
+      else {
+        $scope.message= "Too much!";
+      }
+    }
+  }
+}
+
+}) ();
